c12bcdf8bd1dd52475d535aaffce92d7a619af22
ajgb-eu <135506242+ajgb-eu@users.noreply.github.com>
2025-04-13 20:00:04 +0200

    Initial Commit

---
+++ SoilHue/SoilHueApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoilHueApp.swift
+//  SoilHue
+//
+//  Created by Antonio J. González on 13/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SoilHueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
